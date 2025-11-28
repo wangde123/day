9823a0f82279954b0ff3b200e0a4a8b5d1a23176
v-wangde <v-wangde@xiaomi.com>
2025-11-28 16:54:49 +0800

    feat:添加配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/day/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/day/' : '/',
+  history: { type: 'hash' },
   routes: [
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
